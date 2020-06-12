1a9a2a8a87aa18e1b299ba2a1e892f919cd92aec
Saswat Behera <behera.saswat@gmail.com>
2020-06-12 18:51:40 +0530

    Added Order Object to Model

---
+++ ProcessOrdersWithRules/ProcessOrdersWithRules/Models/Order.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Maersk_BusinessRulesEngine.Models
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public string OrderNumber { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerDetails { get; set; }
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+    }
+}
