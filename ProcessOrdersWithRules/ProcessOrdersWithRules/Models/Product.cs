d9eaf54468215780abf8800f0d35da89100cb2d3
Saswat Behera <behera.saswat@gmail.com>
2020-06-12 18:50:03 +0530

    Added Product Object to Model

---
+++ ProcessOrdersWithRules/ProcessOrdersWithRules/Models/Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Maersk_BusinessRulesEngine.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public bool IsPhysicalProduct { get; set; }
+        public bool IsMembershipProduct { get; set; }
+        public bool IsVideoProduct { get; set; }
+        public bool IsBookProduct { get; set; }
+        public int ProductPrice { get; set; }
+        public string ProductMembershipType { get; set; }
+    }
+
+    enum PackingSlip
+    {
+        CustomerShipping,
+        RoyaltyDepartment
+    }
+
+    enum Membership
+    {
+        Activate,
+        Upgrade
+    }
+}
