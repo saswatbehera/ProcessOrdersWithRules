49ab34c97ef1aa23fd2718c6138de3f6ae503b17
Saswat Behera <behera.saswat@gmail.com>
2020-06-12 18:54:02 +0530

    Added Executables Object to Model

---
+++ ProcessOrdersWithRules/ProcessOrdersWithRules/Models/Executables.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Maersk_BusinessRulesEngine.Models
+{
+    public class Executables
+    {
+        public bool IsPacking { get; set; }
+        public List<Product> Products { get; set; }
+        public bool IsPackingSlipRequired { get; set; }
+        public List<string> PackingSlips { get; set; }
+        public bool IsPhysicalProduct { get; set; }
+        public int AgentCommission { get; set; }
+        public bool IsMembership { get; set; }
+        public List<string> MembershipType { get; set; }
+    }
+}
